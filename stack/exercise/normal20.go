403701435b9a641b2ab866a72c1ca41bb1c5789a
han <hanpp123123@gmail.com>
2024-04-16 11:09:01 +0800

    use stack to resolve leetcode20

---
+++ stack/exercise/normal20.go
@@ -0,0 +1,43 @@
+package exercise
+// 使用一个栈实现解决
+
+func isValid(s string) bool {
+    // 使用slice实现stack结构
+    var sli []rune
+    // 暂存前一个字符
+    var tmp rune
+    for _, value := range s {
+        if value == '{' || value == '(' || value == '[' {
+            sli = append(sli, value)
+            tmp = value
+        }else if len(sli)>0 && value == '}' && tmp == '{' {
+            // pop stack
+			// [:]的方式不会造成数组越界？sli[len(sli)-1]的方式会造成数组越界？
+            sli = sli[:len(sli)-1]
+			if len(sli)>0{
+				tmp = sli[len(sli)- 1]
+			}
+        }else if len(sli)>0 && value == ')' && tmp == '(' {
+            // pop stack
+            sli = sli[:len(sli)-1]
+			if len(sli)>0{
+				tmp = sli[len(sli)- 1]
+			}
+        }else if len(sli)>0 && value == ']' && tmp == '[' {
+            // pop stack
+            sli = sli[:len(sli)-1]
+			if len(sli)>0 {
+				tmp = sli[len(sli)- 1]
+			}        
+		}else {
+            return false
+        }
+    }
+    fmt.Printf("sli len is %d, content is %[1]v", sli)
+    if len(sli) != 0{
+        return false
+    }else {
+        return true
+    }
+    
+}
