94ddc605810cba3ebe9e670932aa3d6b8575cd10
han <hanpp123123@gmail.com>
2024-05-30 14:35:05 +0800

    modified:   skill/search/exercise/leetcode33.go

--- skill/search/exercise/leetcode33.go
+++ skill/search/exercise/leetcode33.go
@@ -13,64 +13,67 @@ import "fmt"
 */
 
 func search(nums []int, target int) int {
-    min := findK(nums)
-    fmt.Println("k index is ", min)
-    res1 := binarySearch(nums, 0, min-1, target)
-    if res1 != -1 {
-        return res1
-    }else {
-        res2 := binarySearch(nums, min, len(nums) - 1, target)
-        if res2 != -1 {
-            return res2
-        }
-    }
-    return -1
+	min := findK(nums)
+	fmt.Println("k index is ", min)
+	res1 := binarySearch(nums, 0, min-1, target)
+	if res1 != -1 {
+		return res1
+	} else {
+		res2 := binarySearch(nums, min, len(nums)-1, target)
+		if res2 != -1 {
+			return res2
+		}
+	}
+	return -1
 }
 
 func findK(data []int) int {
-    left, right := 0, len(data) - 1
-    for left <= right {
-        mid := left + (right - left) / 2
-        // mid 在后半段
-        if data[mid] == data[len(data) - 1] {
-            return len(data) - 1
-        }else if data[mid] < data[len(data) - 1] {
-            right = mid - 1
-        // mid 在前半段
-        }else {
-            left = mid + 1
-        }
-    }
-    return left
+	left, right := 0, len(data)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		// mid 在后半段
+		if data[mid] == data[len(data)-1] {
+			return len(data) - 1
+		} else if data[mid] < data[len(data)-1] {
+			right = mid - 1
+			// mid 在前半段
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
 
 }
-
-
 
 // 最普通的二分查找操作
-func binarySearch(data []int, left, right, target int) int  {
-    for left <= right {
-        mid := left + (right - left) / 2
-        if data[mid] == target {
-            return mid
-        }else if data[mid] < target {
-            left = mid + 1
-        }else {
-            right = mid - 1
-        }
-    }
-    return -1
+func binarySearch(data []int, left, right, target int) int {
+	for left <= right {
+		mid := left + (right-left)/2
+		if data[mid] == target {
+			return mid
+		} else if data[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return -1
 }
 
-func main()  {
-	s := []int{8,9,1,2,3,4,5,6,7}
+func main() {
+	s := []int{8, 9, 1, 2, 3, 4, 5, 6, 7}
 	search(s, 5)
 }
-
 
 /*
 解题思路:
 先找出K所在的索引，k之前是一个有序数组，k之后也是一个有序数组，然后分别对两个数组使用二分查找。
 因为题目规定了时间复杂度是O(logn),所以在找出K的步骤中，也应该使用二分来查找，直接遍历的话是O(n)!
+
+*/
+
+/*
+总结:
+做完几道二分查找的题目后，感觉二分查找的题目考察的是我们对于left、right指针、以及区间开闭的理解，这三个因素与最后return的值息息相关
 
 */
